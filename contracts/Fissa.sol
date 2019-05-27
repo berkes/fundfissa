42a1199b4bec95b12ac70eddba0960ff2bee1597
Bèr Kessels <ber@berk.es>
2019-05-27 21:01:51 +0200

    Add withdraw function.

--- contracts/Fissa.sol
+++ contracts/Fissa.sol
@@ -5,10 +5,11 @@ contract Fissa {
   uint public startsAt = 0;
   uint public ticketPrice = 0;
   uint public threshold = 0;
+  address public organizer;
 
   // @dev: we have a severe security issue here, if we don't explicitely
   //       check the uint8 size on setting it.
-  mapping(address => uint) public balances;
+  mapping(address => uint) private _balances;
 
   event Purchase(address purchaser);
 
@@ -20,6 +21,7 @@ contract Fissa {
     startsAt = _startsAt;
     ticketPrice = _ticketPrice;
     threshold = _threshold;
+    organizer = msg.sender;
   }
 
   function isExpired() public view returns(bool) {
@@ -31,14 +33,35 @@ contract Fissa {
   }
 
   function participants(address participant) public view returns(uint) {
-    return balances[participant] / ticketPrice;
+    return _balances[participant] / ticketPrice;
+  }
+
+  function balances(address participant) public view returns(uint) {
+    if (participant == organizer) {
+      return address(this).balance;
+    }
+    else {
+      return _balances[participant];
+    }
   }
 
   function purchase() public payable {
     require(msg.value == ticketPrice, "TicketPriceMismatch");
     require(! isExpired(), "Expired");
 
-    balances[msg.sender] += msg.value;
+    _balances[msg.sender] += msg.value;
     emit Purchase(msg.sender);
+  }
+
+  function withdraw(uint _amount) public {
+    require(isExpired(), "NotExpired");
+
+    if (!(msg.sender == organizer)) {
+      require(_balances[msg.sender] >= _amount, "InsufficientBalance");
+    }
+
+    _balances[msg.sender] -= _amount;
+
+    msg.sender.transfer(_amount);
   }
 }
--- test/FissaTest.js
+++ test/FissaTest.js
@@ -59,6 +59,10 @@ contract("Fissa", accounts => {
       expect(await fissa.balances.call(constants.ZERO_ADDRESS)).
         to.be.bignumber.equal('0')
     });
+
+    it('has an organizer', async () => {
+      expect(await fissa.organizer.call()).to.equal(roles.organizer);
+    });
   })
 
   describe('purchase', async () => {
@@ -176,4 +180,119 @@ contract("Fissa", accounts => {
       expect(await fissa.isFunded.call()).to.be.true
     });
   }); // isFunded
+
+
+  describe('withdraw when not isExpired()', function () {
+    let fissa = null;
+
+    before(async () => {
+      fissa = await Fissa.new(
+        eventName,
+        startsAt,
+        ticketPrice,
+        threshold
+      );
+
+      await fissa.purchase({ from: roles.buyer, value: ticketPrice });
+    });
+
+    it('is not allowed for buyer', async () => {
+      expect(await fissa.isExpired.call()).to.be.false
+      await expectRevert(
+        fissa.withdraw(ticketPrice, { from: roles.buyer }),
+        "NotExpired"
+      );
+    });
+  }); //withdraw when not isExpired()
+
+  describe('withdraw when isExpired() but not isFunded()', async () => {
+    let fissa = null;
+
+    before(async () => {
+      let latest = await time.latest()
+      fissa = await Fissa.new(
+        eventName,
+        latest.add(time.duration.seconds(10)),
+        ticketPrice,
+        threshold
+      );
+
+      await fissa.purchase({ from: roles.buyer, value: ticketPrice });
+
+      await time.increase(time.duration.seconds(12));
+      // Sanity checks
+      expect(await fissa.isExpired.call()).to.be.true
+      expect(await fissa.isFunded.call()).to.be.false
+    });
+
+    it("allows buyer to withdraw from their balance", async() => {
+      let buyerBalance = await balance.tracker(roles.buyer);
+      let contractBalance = await balance.tracker(fissa.address);
+      let gas = ether('0.001');
+
+      // Withdraw half our balance
+      let amount = ticketPrice.divn(2)
+      await fissa.withdraw(amount, { from: roles.buyer });
+
+      // We withdrew half, so have the same amount, half, left.
+      expect(await fissa.balances.call(roles.buyer)).to.be.bignumber.equal(amount);
+
+      expect(await buyerBalance.delta()).to.be.bignumber.closeTo(amount, gas);
+      expect(await contractBalance.delta()).to.be.bignumber.equal(amount.neg());
+    });
+
+    it("does not allow withdrawing more than our balance", async () => {
+      // We should have half ticketPrice left
+      expect(await fissa.balances.call(roles.buyer)).to.be.bignumber.lt(ticketPrice);
+
+      await expectRevert(
+        fissa.withdraw(ticketPrice, { from: roles.buyer }),
+        "InsufficientBalance"
+      );
+    });
+  }); // withdraw when isExpired() but not isFunded()
+
+  describe('withdraw when isExpired() and isFunded()', async () => {
+    let fissa = null;
+
+    before(async () => {
+      let latest = await time.latest()
+      fissa = await Fissa.new(
+        eventName,
+        latest.add(time.duration.seconds(10)),
+        ticketPrice,
+        ticketPrice.muln(2)
+      );
+
+      await fissa.purchase({ from: roles.buyer, value: ticketPrice });
+      await fissa.purchase({ from: roles.buyer, value: ticketPrice });
+      await time.increase(time.duration.seconds(12));
+
+      // Sanity checks
+      expect(await fissa.isExpired.call()).to.be.true
+      expect(await fissa.isFunded.call()).to.be.true
+    });
+
+    it("refutes buyer to withdraw from their balance", async() => {
+    });
+
+    it("sets entire contract balance to organizer", async () => {
+      let amount = ticketPrice.muln(2);
+      let contractBalance = await balance.tracker(fissa.address);
+      expect(await contractBalance.get()).to.be.bignumber.equal(amount)
+      expect(await fissa.balances.call(roles.organizer)).to.be.bignumber.equal(amount);
+    });
+
+    it("allows organizer to withdraw balance", async () => {
+      let contractBalance = await balance.tracker(fissa.address);
+
+      // Withdraw half our balance: one ticketPrice
+      let amount = ticketPrice
+      await fissa.withdraw(amount, { from: roles.organizer });
+
+      expect(await fissa.balances.call(roles.organizer)).to.be.bignumber.equal(amount);
+      expect(await contractBalance.delta()).to.be.bignumber.equal(amount.neg());
+      expect(await fissa.balances.call(roles.organizer)).to.be.bignumber.equal(amount);
+    });
+  }); // withdraw when isExpired() but not isFunded()
 });
