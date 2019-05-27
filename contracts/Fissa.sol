7adba118a700b648156d4675e3b775b2c89dadac
Bèr Kessels <ber@berk.es>
2019-05-27 15:29:32 +0200

    Introduce a purchase function allowing us to buy tickets.

--- contracts/Fissa.sol
+++ contracts/Fissa.sol
@@ -6,6 +6,12 @@ contract Fissa {
   uint public ticketPrice = 0;
   uint public threshold = 0;
 
+  // @dev: we have a severe security issue here, if we don't explicitely
+  //       check the uint8 size on setting it.
+  mapping(address => uint8) public participants;
+
+  event Purchase(address purchaser);
+
   constructor(string memory _eventName,
               uint _startsAt,
               uint _ticketPrice,
@@ -18,5 +24,12 @@ contract Fissa {
 
   function isExpired() public view returns(bool) {
     return startsAt < now;
+  }
+
+  function purchase() public payable {
+    require(msg.value == ticketPrice, "TicketPriceMismatch");
+
+    participants[msg.sender] += 1;
+    emit Purchase(msg.sender);
   }
 }
--- test/FissaTest.js
+++ test/FissaTest.js
@@ -1,7 +1,9 @@
 const Fissa = artifacts.require("./Fissa.sol");
 const { expect } = require('chai');
-const { BN,
+const { balance,
+        BN,
         constants,
+        ether,
         expectEvent,
         expectRevert,
         time } = require('openzeppelin-test-helpers');
@@ -9,8 +11,12 @@ const { BN,
 contract("Fissa", accounts => {
   let eventName = "Huisfeest";
   let startsAt =  Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 5);
-  let ticketPriceInEth = 0.2;
-  let thresholdInEth = 1.2; //deliberately devidable by 0.2
+  let ticketPrice = ether('0.2');
+  let threshold = ether('1.2'); //deliberately devidable by 0.2
+  let roles = {
+    organizer: accounts[0],
+    buyer: accounts[1]
+  }
 
   describe("deploy", () => {
     let fissa = null;
@@ -19,8 +25,8 @@ contract("Fissa", accounts => {
       fissa = await Fissa.new(
         eventName,
         startsAt,
-        web3.utils.toWei(ticketPriceInEth.toString()),
-        web3.utils.toWei(thresholdInEth.toString())
+        ticketPrice,
+        threshold
       );
     });
 
@@ -36,13 +42,18 @@ contract("Fissa", accounts => {
 
     it("sets a ticketPrice in wei", async () => {
       const actualTicketPrice = await fissa.ticketPrice.call();
-      assert.equal(web3.utils.fromWei(actualTicketPrice), ticketPriceInEth);
-    })
+      expect(actualTicketPrice).to.be.bignumber.equal(ticketPrice);
+    });
 
     it("has a threshold", async () => {
       const actualThreshold = await fissa.threshold.call();
-      assert.equal(web3.utils.fromWei(actualThreshold), thresholdInEth);
-    })
+      expect(actualThreshold).to.be.bignumber.equal(threshold);
+    });
+
+    it('has a participants map', async () => {
+      expect(await fissa.participants.call(constants.ZERO_ADDRESS)).
+        to.be.bignumber.equal('0')
+    });
   })
 
   describe('expired', async () => {
@@ -53,8 +64,8 @@ contract("Fissa", accounts => {
       fissa = await Fissa.new(
         eventName,
         latest.add(time.duration.seconds(10)),
-        web3.utils.toWei(ticketPriceInEth.toString()),
-        web3.utils.toWei(thresholdInEth.toString())
+        ticketPrice,
+        threshold
       );
       await time.increase(time.duration.seconds(12));
     });
@@ -70,4 +81,58 @@ contract("Fissa", accounts => {
       expect(await fissa.isExpired.call()).to.be.true
     })
   });
+
+  describe('purchase', async () => {
+    let fissa = null;
+
+    before(async () => {
+      fissa = await Fissa.new(
+        eventName,
+        startsAt,
+        ticketPrice,
+        threshold
+      );
+    });
+
+    it('adds buyer to list of participants', async () => {
+      await fissa.purchase({ from: roles.buyer, value: ticketPrice });
+      expect(await fissa.participants(roles.buyer)).to.be.bignumber.equal('1');
+    });
+
+    it('increments buyer amount in participants', async () => {
+      // We have 1 from previous testcase. Yes. This sucks ¯\_(ツ)_/¯.
+      await fissa.purchase({ from: roles.buyer, value: ticketPrice });
+      expect(await fissa.participants(roles.buyer)).to.be.bignumber.equal('2');
+    });
+
+    it('transfers ether from buyer into the contract', async () => {
+      let buyerBalance = await balance.tracker(roles.buyer);
+      let contractBalance = await balance.tracker(fissa.address);
+
+      await fissa.purchase({ from: roles.buyer, value: ticketPrice });
+
+      let gas = ether('0.001');
+      expect(await buyerBalance.delta()).to.be.bignumber.closeTo(ticketPrice.neg(), gas);
+      expect(await contractBalance.delta()).to.be.bignumber.equal(ticketPrice);
+    });
+
+    it('does not allow payments lower than ticketPrice', async () => {
+      await expectRevert(
+        fissa.purchase({ from: roles.buyer, value: ether('0.1') }),
+        "TicketPriceMismatch"
+      )
+    });
+
+    it('does not allow payments higher than ticketPrice', async () => {
+      await expectRevert(
+        fissa.purchase({ from: roles.buyer, value: ether('0.3') }),
+        "TicketPriceMismatch"
+      )
+    });
+
+    it('sends a Purchase event', async () => {
+      const { logs } = await fissa.purchase({ from: roles.buyer, value: ticketPrice });
+      expectEvent.inLogs(logs, 'Purchase', { purchaser: roles.buyer });
+    });
+  }); // purchase
 });
